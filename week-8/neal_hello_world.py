6863fd39da66056ffba2abf1c16951cc5e9fe06b
Jourdan Neal <jjjet1991@gmail.com>
2021-07-16 04:50:16 -0700

    adding week 8

---
+++ week-8/neal_hello_world.py
@@ -0,0 +1,11 @@
+#
+#=====================================================
+# Title: Assignment 8.2 Anaconda  
+# Author: Professor Krasso
+# Date: 16 July 2021
+# Modified By: Jourdan Neal
+# Description: Printing Hello World to debug console.
+#=====================================================
+#
+
+print('Hello World')
