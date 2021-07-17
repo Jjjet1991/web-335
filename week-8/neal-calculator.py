3b7ba8e7c75ecd621d5fbc6077381ccaa75e4c43
Jourdan Neal <jjjet1991@gmail.com>
2021-07-17 01:26:24 -0700

    adding 8.2

---
+++ week-8/neal-calculator.py
@@ -0,0 +1,26 @@
+#
+#=====================================================
+# Title: Assignment 8.3 Python in Action 
+# Author: Professor Krasso
+# Date: 17 July 2021
+# Modified By: Jourdan Neal
+# Description: Create basic functions and print results using python
+#=====================================================
+#
+
+# Create add function w/ 2 parameters
+def add (a,b):
+    return a + b
+
+# Create subtract function w/ 2 parameters
+def subtract (a,b):
+    return a-b
+
+# Create divide function w/ 2 parameters
+def divide (a,b):
+    return a/b
+
+# Call seperate print statement for each function
+print(add(1,2))
+print(subtract(4,1))
+print(divide(8,2))
